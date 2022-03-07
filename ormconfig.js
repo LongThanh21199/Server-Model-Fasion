25bbcdd292096fc36d6099fc0cf2b6764fe0a7ba
BuiThanhLong <lbui21199@gmail.com>
2022-03-07 14:47:31 +0700

    Config database

--- ormconfig.js
+++ ormconfig.js
@@ -1,7 +1,7 @@
 module.exports = {
    "type": "postgres",
-   "host": "localhost",
-   "port": 5432,
+   "host": process.env.DATABASE_HOST,
+   "port": process.env.DATABASE_PORT,
    "username": process.env.POSTGRES_USER,
    "password": process.env.POSTGRES_PASSWORD,
    "database": process.env.POSTGRES_DB,
